1e522b809f1856ce7a0ac4c15e470f7e3d61e5e2
Mariano Arias <hola@marianoarias.soy>
2021-11-08 11:46:12 -0300

    Removed files from the index (now ignored)

---
+++ database/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+   host: 'j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+   user: 'olqx4wg5brri3ppp',
+   password: 'g5x0orwtlzzqimo6',
+   database: 'yh24of7qy8a35wgm'
+});
+
+
+// const connection = mysql.createConnection({
+//    host: 'localhost',
+//    user: 'root',
+//    password: 'Verano22',
+//    database: 'exincor'
+// });
+
+connection.connect((err) => {
+   if (err) throw err;
+   console.log("¡Conexión establecida!");
+});
+
+module.exports = connection;
