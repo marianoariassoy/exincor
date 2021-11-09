2da3a40189009274db729f8442182b3e3b4bcd24
Mariano Arias <hola@marianoarias.soy>
2021-11-08 22:20:55 -0300

    Con nodemailer

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 
-//Muestra y cierra el nav
+// Muestra y cierra el nav
 const menu = document.getElementById("menu");
 const close = document.getElementById("close");
 const nav = document.getElementById("nav");
@@ -11,3 +11,34 @@ menu.addEventListener("click", function(evento){
 close.addEventListener("click", function(evento){
    nav.style.display = 'none';
 });
+
+// Validación del formaulario
+function formulario() {
+   let nombre = document.getElementById("nombre").value;
+   let apellido = document.getElementById("apellido").value;
+   let email = document.getElementById("email").value;
+   let celular = document.getElementById("celular").value;
+   let mensaje = document.getElementById("mensaje").value;
+
+   if (nombre == null || nombre.length == 0) {
+      alert("Debe ingresar su nombre");
+      return false;
+   }
+   if (apellido == null || apellido.length == 0) {
+      alert("Debe ingresar su apellido");
+      return false;
+   }
+   if (!/\S+@\S+\.\S+/.test(email)) {
+      alert("Debe ingresar un correo válido");
+      return false;
+   }
+   if (celular == null || celular.length == 0 || isNaN(celular)) {
+      alert("Debe ingresar un número de telefono");
+      return false;
+   }
+   if (mensaje == null || mensaje.length == 0) {
+      alert("Debe ingresar un mensaje")
+      return false;
+   }
+   return true;
+}
--- router.js
+++ router.js
@@ -1,8 +1,9 @@
-//rutas
+// Rutas
 const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
 
-//base de datos
+// Base de datos
 const connection = require("./database/db");
 
 router.get("/", function (req, res) {
@@ -45,6 +46,46 @@ router.get("/galeria", function (req, res) {
 
 router.get("/contacto", function (req, res) {
    res.render("contacto");
+});
+
+// Envio email
+router.post("/send_mail", function (req, res) {
+   const nombre = req.body.nombre;
+   const apellido = req.body.apellido;
+   const celular = req.body.celular;
+   const email = req.body.email;
+   const ciudad = req.body.ciudad;
+   const pais = req.body.pais;
+   const mensaje = req.body.mensaje;
+
+   const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+         user: 'edwardo.kutch82@ethereal.email',
+         pass: 'c1r3q1PT8QuCy4hCsJ'
+      }
+   });
+
+   const mailOptions = {
+      form: email,
+      to: "hola@marianoarias.soy",
+      subject: "Contacto",
+      html: `Nombre: <strong>${nombre} ${apellido}</strong> <br>
+      Celular: <strong>${celular}</strong>, Email: <strong>${email}</strong> <br> 
+      Ciudad: <strong>${ciudad}</strong>,  Pais: <strong>${pais}</strong> <br> 
+      Mensaje: <strong>${mensaje}</strong>`
+   };
+
+   transporter.sendMail(mailOptions, (err, info)=>{
+      if(err) {
+         res.status(500).send(err.message);
+      } else {
+         console.log("Email enviado");
+         res.render("contacto_ok");
+      }
+   }) 
+
 });
 
 router.get("*", function (req, res) {
